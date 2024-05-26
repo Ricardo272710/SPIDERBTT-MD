3f0f23eddb0eed3ce46719d2a2eba639cdb178b7
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-25 22:37:12 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,9 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
  global.owner = [
-['595972157130', '🍁 𝗢𝘄𝗻𝗲𝗿 🐼', true]]
+['595972157130', 'Ivan🇵🇾', true],
+['51929972576', 'JXSTXN🇵🇪', true],
+['5491168758497', 'Enzito🇦🇷', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
