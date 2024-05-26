4fe53caad2fadd780d79f4e1f7aebabe0f2eff70
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-25 23:04:59 -0400

    Actualizar info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,35 +1,6 @@
 var handler  = async (m, { conn }) => {
 
-let texto = `🚩 *Instalación de DiablaBot-MD*
-
-*Requisitos para la instalación*
-⬡ Dudas: wa.me/5491130182250
-⬡ 1 GB de almacenamiento
-⬡ Termux: https://www.mediafire.com/file/3hsvi3xkpq3a64o/termux_118.apk/file
-⬡ GitHub: https://github.com/EnzoVaselevich/DiablaBot-MD-oficial
-⬡ Un whatsapp inmune (secundario)
-⬡ Un número 
-⬡ Dispositivo o una PC para escanear
-
-*Comandos de instalación via Termux*
-
-termux-setup-storage
-
-apt-get update -y && apt-get upgrade -y
-
-pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn
-
-git clone https://github.com/EnzoVaselevich/DiablaBot-MD-oficial && cd DiablaBot-MD-oficial && yarn install && npm install 
-
-ls
-
-npm start
-
-_Utilice "comandos" para enviarle los comandos uno por uno_
-
-_Utilice "instalar2" para ver los otros tipos de instalación_
-
-_Utilice "vortexus" para instalar el bot mediante el hosting vortexus_`
+let texto = `*Escribe con el propietario si quieres serbot o donar numeros*`
 
 conn.reply(m.chat, texto, m, fake, )
 
