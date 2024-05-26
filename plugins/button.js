09f957d1e4d91ba7382933e42c3232e7905e67c9
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-25 21:26:18 -0400

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -6,9 +6,8 @@ await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto,[
 ['ꨄ 𝐌𝐞𝐧𝐮 𝐜𝐨𝐦𝐩𝐥𝐞𝐭𝐨 🌸', '!allmenu'],
 ['ꨄ 𝐃𝐢𝐚𝐛𝐥𝐚𝐁𝐨𝐭-𝐌𝐃 🌹', '!sc'],
 ['ꨄ 𝐃𝐞𝐬𝐚𝐫𝐫𝐨𝐲𝐚𝐝𝐨𝐫𝐞𝐬 ✨', '#owner'],
-['ꨄ 𝐃𝐢𝐚𝐛𝐥𝐚𝐁𝐨𝐭 💥', '.bot Hola']], null, [
+['ꨄ Creditos a Diego💥', '.bot Hola']], null, [
 ['ꨄ Canal Oficial', `${channel}`]], m)}
-'Creditos a diego'
 handler.customPrefix = /Ivancito|Ivan/i;
 handler.command = new RegExp;
 export default handler;
