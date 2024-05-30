720bf17455105a675b301910fca0814e375faa4f
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-30 18:58:54 -0400

    Create Drive.js

---
+++ plugins/Drive.js
@@ -0,0 +1,26 @@
+import fg from 'api-dylux'  
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+	if (!args[0]) throw `✳️ Ingrese un link de Google Drive`
+	m.react(rwait) 
+	try {
+	let res = await fg.GDriveDl(args[0])
+	 await m.reply(`
+≡ *Google Drive DL*
+▢ *Nombre:* ${res.fileName}
+▢ *Tamaño:* ${res.fileSize}
+▢ *tipo:* ${res.mimetype}`)
+		
+	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
+	m.react(done)
+   } catch {
+	m.reply('Error: Revisa el link o intenta con otro link') 
+  }
+}
+handler.help = ['gdrive']
+handler.tags = ['dl', 'prem']
+handler.command = ['gdrive']
+handler.diamond = true
+handler.premium = true
+
+export default handler
