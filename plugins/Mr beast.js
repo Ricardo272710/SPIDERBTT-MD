c3e1d2010c6776192bad31ce5ca95ad08a392804
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-31 15:01:35 -0400

    Create Mr beast.js

---
+++ plugins/Mr beast.js
@@ -0,0 +1,9 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.millon);
+  m.reply(global.millon);
+};
+handler.command = /^(Rico|Millon|ser)$/i;
+export default handler;
+
+global.millon = `*Felicitaciones te haz ganado 1M de Dólares 
+_atte:Mr Beast_`;
