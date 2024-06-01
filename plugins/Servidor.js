3ee33227588b9babcc90c3a2d7177c9fe80ea2e6
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-31 21:48:13 -0400

    Create Servidor.js

---
+++ plugins/Servidor.js
@@ -0,0 +1,45 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let txt = `*\`💫 Te presentamos un nuevo server: "codesandbox"\`*
+
+*¿Muy lento tu nokia y necesitas tener activo tu bot 24/7?*
+
+> *Te tenemos la mejor opción para mantener activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
+
+Contamos con:
+
+Plan: Free
+Para: Todos usuarios
+
+Plan: Pro
+Para: Usuarios que paguen
+Precio: 12$ USD
+
+Plan: Enterprise - Empresas
+Para: Empresas
+Precio: Solo acordado y dado por el creador nada mas
+
+🔵 \`\`\`Información del Server\`\`\`
+
+💻 *Página:*
+https://codesandbox.io/dashboard
+
+💙 *Canal de WhatsApp:*
+https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K`
+await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `🌻 CODESANDBOX 🍁`,
+body: `✅ Hosting de Calidad`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://telegra.ph/file/37478c04c4abeb4d02f80.jpg', 
+sourceUrl: 'https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K'}}},
+{ quoted: fkontak})
+}
+handler.tags =['info'] 
+handler.help = ['codesandbox', 'Servidor', 'Obte'] 
+handler.command = /^(sandbox|box|sand|server)$/i
+export default handler
