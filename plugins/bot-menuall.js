94166deb2a3a10a330e38a2ea7073c95c3aafc07
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-31 21:11:03 -0400

    Actualizar bot-menuall.js

--- plugins/bot-menuall.js
+++ plugins/bot-menuall.js
@@ -168,8 +168,8 @@ const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://teleg
 
 await conn.reply(m.chat, '✰【𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐄𝐥 𝐌𝐞𝐧𝐮】✰ . . .*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '🍁 Goku-Black-Bot-MD 🌸', body: '👋 Hola ' + name, sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
     
-    let pp1 = 'https://telegra.ph/file/a318a2f96918255a8981d.mp4'
-    let pp2 = 'https://telegra.ph/file/a318a2f96918255a8981d.mp4'
+    let pp1 = 'https://telegra.ph/file/e1f516b1abcbc07e4463b.mp4'
+    let pp2 = 'https://telegra.ph/file/e1f516b1abcbc07e4463b.mp4'
 
     m.react('🌸') 
     conn.sendMessage(m.chat, { video: { url: [pp1, pp2].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m }) 
