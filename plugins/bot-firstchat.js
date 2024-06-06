7886a61495e7692e574e4f7c5b2f2f11e6661157
Ivan <162828351+elias1524@users.noreply.github.com>
2024-06-06 14:42:01 -0400

    Actualizar bot-firstchat.js

--- plugins/bot-firstchat.js
+++ plugins/bot-firstchat.js
@@ -17,7 +17,7 @@ await m.reply(`👋 𝙷𝙾𝙻𝙰 ${nombre}!!
 *◉ 𝙴𝚂𝙲𝚁𝙸𝙱𝙰 .𝙼𝙴𝙽𝚄́ 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂*
   
 *◉ ¿𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 𝙴𝚂𝚃𝙴 𝙿𝚁𝙾𝚈𝙴𝙲𝚃𝙾 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝚂𝙸𝙶𝙰 𝙰𝙲𝚃𝚄𝙰𝙻𝙸𝚉𝙰́𝙽𝙳𝙾𝚂𝙴?* 
-*◉ 𝙿𝚄𝙴𝙳𝙴𝚂 𝙰𝙿𝙾𝚈𝙰𝚁𝙽𝙾𝚂 𝙳𝙴𝙹𝙰𝙽𝙳𝙾 𝚄𝙽𝙰 𝙴𝚂𝚃𝚁𝙴𝙻𝙻𝙸𝚃𝙰 ⭐ 𝙰𝙻 𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾.
-*https://github.com/EnzoVaselevich/DiablaBot-MD-oficial*`) 
+*◉ 𝙿𝚄𝙴𝙳𝙴𝚂 𝙰𝙿𝙾𝚈𝙰𝚁𝙽𝙾𝚂 𝙳𝙴𝙹𝙰𝙽𝙳𝙾 una 🌟 
+*Puedes donarme números también al wa.me/595972157130*`) 
 user.pc = new Date * 1
 }
