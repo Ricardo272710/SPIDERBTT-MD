777eae77d2d651d4cd1f86c6b5e4579689334699
Ivan <162828351+elias1524@users.noreply.github.com>
2024-06-24 15:42:46 -0400

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,11 +1,11 @@
 var handler = async (m, { conn, command }) => {
 
-let str = `> 👑 Gracias por apoyar este bot
-> ✨ Gracias a tu colaboración 🪙 el bot seguira en constantes actualizaciónes mejoradas 🎨, puede comprar servidores tambien, 🟢 funcionalidad, 👥 grupos con bot, ✅ rapido y seguro, 🤖 bot en 24/7, 🌍 precios aceptables, 🍷 mas funciones solo lo podras encontrar en: 🌌 \`𝙶𝚘𝚔𝚞-𝙱𝚕𝚊𝚌𝚔-𝙱𝚘𝚝-𝙼𝙳\` 🌌
+let str = `*👑 Gracias por apoyar este bot*
+*✨ Gracias a tu colaboración 🪙 el bot* seguira en constantes actualizaciónes mejoradas 🎨, puede comprar servidores tambien, 🟢 funcionalidad, 👥 grupos con bot, ✅ rapido y seguro, 🤖 bot en 24/7, 🌍 precios aceptables, 🍷 mas funciones solo lo podras encontrar en: 🌌 \`𝙶𝚘𝚔𝚞-𝙱𝚕𝚊𝚌𝚔-𝙱𝚘𝚝-𝙼𝙳\` 🌌
 
-> Puedes Donarme números también 
+*Puedes Donarme números también*
 
-> \`Mandar pruebas en captura:\`
+*\`Mandar pruebas en captura:\`*
 > https://wa.me/595972157130`
 
 conn.reply(m.chat, str, m, fake, )
