550bfbbfdf8578e80f621e23213ca839a0aba133
Ivan <162828351+elias1524@users.noreply.github.com>
2024-06-24 15:49:56 -0400

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
 let text = `❤️ Ese es mi propietario.`
 await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto,[
-['ꨄ 𝐌𝐞𝐧𝐮 𝐜𝐨𝐦𝐩𝐥𝐞𝐭𝐨 🌸', '!allmenu'],
+['ꨄ Menu', '!allmenu'],
 ['ꨄ Goku-Black-Bot-MD 🌹', '!sc'],
-['ꨄ 𝐃𝐞𝐬𝐚𝐫𝐫𝐨𝐲𝐚𝐝𝐨𝐫𝐞𝐬 ✨', '#owner'],
+['ꨄ Desarolladores ✨', '#owner'],
 ['ꨄ Goku-Black-Bot-MD💥', '.bot Hola']], null, [
 ['ꨄ Canal Oficial', `${channel}`]], m)}
-handler.customPrefix = /Ivancito|Ivan/i;
+handler.customPrefix = /Elias|Eliass/i;
 handler.command = new RegExp;
 export default handler;
