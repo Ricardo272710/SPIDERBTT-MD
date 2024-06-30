ae0f7e8f5d6812b85d00c35bcc2a6b72f77dc963
Ivan <162828351+elias1524@users.noreply.github.com>
2024-06-30 13:03:02 -0400

    Actualizar Servidor.js

--- plugins/Servidor.js
+++ plugins/Servidor.js
@@ -1,66 +1,39 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `PARA LOS QUE QUIERAN PATROCINARNOS EN SUS PROYECTOS
-
-*✨ HOLA BIENVENIDO/A TE PRESENTO ~CODESANDBOX~*
-
-*¿QUE ES CODESANDBOX?*
-_PLATAFORMA DONDE PUEDES ALOJAR MÚLTIPLES PROYECTOS A LARGO ALCANZE CON MUCHOS IDIOMAS Y CUSTOMIZADO_
-
-*¿QUE VENTAJAS TIENE Y QUE DIFERENCIA DE OTROS HOTS?*
-_LA DIFERENCIA ES QUE LA MAYORIA DE HOST MANTIENEN SOLO LOS LENGUAJES DE PROGRAMACION HABITUALES COMO "JAVASCRIPT" - "HTML" - "PYTHON" ADEMAS DE QUE ESTOS PUEDEN COSTAR UN CIERTO COSTO Y/O ALCANCE ADEMAS DE QUE NO PUEDES ALOJAR TANTOS DEBIDO A ALGUNA LIMITACIÓN O SU DEBIDO DINERO._
-
-_CODESANDBOX ES UN LUGAR GRATUITO DONDE TAMBIÉN ADEMAS DE QUE TENGAS UNA CUENTA, PUEDES CONECTAR TUS PROYECTOS PRIVADOS/PUBLICOS DE GITHUB, AMPLIO SISTEMA DE SOPORTE Y CUSTOMIZACION_
-
-_🌟 CONTAMOS CON SOPORTE GARANTIZADO Y EXTRA_
-
-_CONTAMOS CON AMPLIA VARIEDAD DE FORMATOS O LENGUAJES COMO PHP / PYTHON / JAVA / JAVASCRIPT / TYPESCRIPT / REACT / ANGULAR / ETC_
-
-*¿PUEDO LLEGAR A PAGARLO O OBTENER OTRAS FUNCIONES?*
-_SI, CONTAMOS CON UNA VERSION "VIP" O "PRO" EL CUAL PERMITE AL USUARIO  USAR LENGUAJES PERZONALIZADOS O DIRECTAMENTE CARGAR SU PROPIO SISTEMA DE LENGUAJE Y UN PROPIO LUGAR DONDE PODRAS TENER TUS PROYECTOS Y SISTEMA PRIVADOS CON MAS FUNCIONES._
-
-*¿COMO PUEDO INICIAR?*
-_INICIAS O ALOJAS DE FORMA GRATUITA CON 4GB DE ESPACIO PARA CUALQUIER ESPECIALIDAD Y SI QUIERES AUMENTARLO ES PAGANDO DE FORMA VIP O EN SORTEOS OFICIALES_
-
-*_PAGINA_*
-_https://codesandbox.io/dashboard_
-
-*_CANAL DE WHATSAPP_*
-_https://whatsapp.com/channel/0029VagAcYlGE56u6WANKc2w_
-
-*_CREADOR_*
-_wa.me/5493795319022_
-
-*_PAGOS PARA EL VIP_*
-_wa.me/5493795297363_
-*\`🍁 Te presentamos un nuevo server: "codesandbox"\`*
-
-*¿Muy lento tu Samsung J1 y necesitas tener activo tu bot 24/7?*
-
-> *Te tenemos la mejor opción para que tengas activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
-
-Contamos con:
-
-Plan: Free
-Para: Todos usuarios
-
-Plan: Pro
-Para: Usuarios que paguen
-Precio: 12$ USD
-
-Plan: Enterprise - Empresas
-Para: Empresas
-Precio: Solo acordado y dado por el creador nada mas
-
-🔵 \`\`\`Información del Server\`\`\`
-
-💻 *Página:*
-https://codesandbox.io/dashboard
-
-💙 *Canal de WhatsApp:*
-https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K
-
-📱 *Contacto*
-wa.me/5493794297363`
+let txt = `╭──────⚔──────╮
+ㅤㅤ12 𝐕𝐄𝐑𝐒𝐔𝐒 12
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
+│ㅤ🇲🇽 𝐌𝐄𝐗 : 
+│ㅤ🇨🇴 𝐂𝐎𝐋 : 
+│➥ 𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│     
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯`
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardingScore: 9999999,
@@ -68,14 +41,14 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🍁 CODESANDBOX 🍁`,
-body: `✔️ Servidor de Calidad`,
+title: ``,
+body: ``,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://telegra.ph/file/37478c04c4abeb4d02f80.jpg', 
-sourceUrl: 'https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K'}}},
+thumbnailUrl: 'https://telegra.ph/file/a76519a488755d9dd6097.jpg', 
+sourceUrl: ''}}},
 { quoted: fkontak})
 }
 handler.tags =['info'] 
 handler.help = ['codesandbox', 'Servidor', 'Obte'] 
-handler.command = /^(sandbox|box|sand|server)$/i
+handler.command = /^(sandbox|12vs12|sand|server)$/i
 export default handler
