4e117c98dbc9fb2bd3ce6655b996ebcecf6d172b
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-30 12:24:34 -0500

    Create freefire-8vs8.js

---
+++ plugins/freefire-8vs8.js
@@ -0,0 +1,48 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let txt = `╭──────⚔──────╮
+ㅤㅤㅤ8 𝐕𝐄𝐑𝐒𝐔𝐒 8
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎  
+│ㅤ🇲🇽 𝐌𝐄𝐗 : 
+│ㅤ🇨🇴 𝐂𝐎𝐋 : 
+│➥ 𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│     
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+               `
+await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: 8vs8,
+body: 8vs8,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://telegra.ph/file/a76519a488755d9dd6097.jpg', 
+sourceUrl: ''}}},
+{ quoted: fkontak})
+}
+handler.tags =['info'] 
+handler.help = ['codesandbox', 'Servidor', 'Obte'] 
+handler.command = /^(sandbox|8vs8|sand|server)$/i
+export default handler
