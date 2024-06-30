a2c774703e1e3fcf81d84cff62c33df93bb8ad88
Ivan <162828351+elias1524@users.noreply.github.com>
2024-06-30 12:56:59 -0400

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,17 +1,17 @@
 var handler = async (m, { conn, command }) => {
 
-let str = `*SPIDER 𝗕𝗼𝘁 🐉\n\n 𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗔𝗽𝗼𝘆𝗮𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗔𝗹𝗴𝘂𝗻 𝗦𝗲𝗿𝘃𝗲𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗢 𝗧𝗯𝗺 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 metodos de apoyar 𝗘𝘀 :
+let str = `*SPIDER 𝗕𝗼𝘁* 🐉\n\n 𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗔𝗽𝗼𝘆𝗮𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗔𝗹𝗴𝘂𝗻 𝗦𝗲𝗿𝘃𝗲𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗢 𝗧𝗯𝗺 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 metodos de apoyar 𝗘𝘀 :
 ✅PAYPAL
 https://paypal.me/RicardoSilva18160
 
  *BANCO PICHINCHA* 
  Cta ahorro 
 3910143100
-Ricardo Silva*
+*Ricardo Silva*
 
-YAPPY PANAMA : 67712442*
+*YAPPY PANAMA* : 67712442*
 
-*NEQUI PANAMÁ :* 65197283*`
+*NEQUI PANAMÁ* : 65197283*`
 
 conn.reply(m.chat, str, m, fake, )
 
